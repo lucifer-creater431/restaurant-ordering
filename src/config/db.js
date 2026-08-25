e3cf5d85aa27b7880d513d6b67cfbda611a4a7ee
lucifer-creater431 <lucifer-creater431@example.com>
2026-08-25 12:10:38 +0500

    Fix DB connection string

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,8 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || '127.0.0.1', // IPv6 connection issue se bachne ke liye 127.0.0.1
-  database: process.env.DB_NAME || 'restaurant_db',
-  password: process.env.DB_PASSWORD || 'postgres',
-  port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
